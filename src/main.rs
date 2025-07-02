b8163b9d6ad99d0aafbbe7acd49c2b97b54e1e95
sokolawesome <sokolawesome@gmail.com>
2025-07-03 01:01:47 +0300

    feat: basic layout and clock widget

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod widgets;
+
 use gtk4::prelude::*;
-use gtk4::{Application, ApplicationWindow};
+use gtk4::{Application, ApplicationWindow, glib};
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
+
+use widgets::clock::ClockWidget;
 
 fn main() {
     let app = Application::builder()
@@ -12,18 +16,13 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("forgebar")
-        .build();
+    let window = ApplicationWindow::builder().application(app).build();
 
     window.init_layer_shell();
     window.set_layer(Layer::Top);
-
     window.set_anchor(Edge::Bottom, true);
     window.set_anchor(Edge::Left, true);
     window.set_anchor(Edge::Right, true);
-
     window.set_namespace(Some("forgebar"));
 
     let main_box = gtk4::Box::new(gtk4::Orientation::Horizontal, 10);
@@ -32,6 +31,25 @@ fn build_ui(app: &Application) {
     main_box.set_margin_top(5);
     main_box.set_margin_bottom(5);
 
+    let left_box = gtk4::Box::new(gtk4::Orientation::Horizontal, 10);
+    let center_box = gtk4::Box::new(gtk4::Orientation::Horizontal, 10);
+    let right_box = gtk4::Box::new(gtk4::Orientation::Horizontal, 10);
+
+    let clock = ClockWidget::new();
+    center_box.append(&clock.widget());
+
+    main_box.append(&left_box);
+    main_box.append(&center_box);
+    main_box.append(&right_box);
+
     window.set_child(Some(&main_box));
     window.present();
+
+    glib::timeout_add_seconds_local(1, move || {
+        let value = clock.clone();
+        glib::MainContext::default().spawn_local(async move {
+            value.update().await;
+        });
+        glib::ControlFlow::Continue
+    });
 }
---
+++ src/widgets/clock.rs
@@ -0,0 +1,27 @@
+use chrono::Local;
+use gtk4::Label;
+
+#[derive(Clone)]
+pub struct ClockWidget {
+    label: Label,
+}
+
+impl ClockWidget {
+    pub fn new() -> Self {
+        let label = Label::new(None);
+        label.set_markup("<span font='16'>Loading...</span>");
+
+        Self { label }
+    }
+
+    pub fn widget(&self) -> Label {
+        self.label.clone()
+    }
+
+    pub async fn update(&self) {
+        let now = Local::now();
+        let formatted_time = now.format("%H:%M:%S | %d.%m.%y").to_string();
+        self.label
+            .set_markup(&format!("<span font='16'>{}</span>", formatted_time));
+    }
+}
